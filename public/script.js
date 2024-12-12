d2deefaa8e4a92923508514fb48325b8de56789c
SamuelTussing <sml.tussing@gmail.com>
2024-12-12 10:51:54 +0100

    changement clé api

--- public/script.js
+++ public/script.js
@@ -36,7 +36,7 @@ fetch('/api/google-maps-key')
   .then((response) => response.json())
   .then((data) => {
     const script = document.getElementById('google-maps-script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg&callback=initMap&libraries=geometry`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAyLL0_201kOZ_7XflG2VVmI0XF7NWMwXk&callback=initMap&libraries=geometry`;
   })
   .catch((error) => console.error('Erreur lors du chargement de la clé API:', error));
 
