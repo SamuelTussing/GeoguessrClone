152579dfcff935ede07180849efb35da35849eff
SamuelTussing <sml.tussing@gmail.com>
2025-01-08 11:20:14 +0100

    modif import clientpromise dans login.js

--- api/login.js
+++ api/login.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import clientPromise from '../../lib/mongodb';
+import clientPromise from '../lib/mondodb';
 
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
