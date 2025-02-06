8dc87396d87ad8eb41c0d6f391333da14a58a56c
SamuelTussing <sml.tussing@gmail.com>
2025-02-06 12:11:40 +0100

    ajout de nouveaux badges

--- public/badgeList.js
+++ public/badgeList.js
@@ -9,5 +9,13 @@ const badgeList = [
     {badgeName : "Niv70", badgesrc:"./badge/70.png", badgeDesc:"Atteindre le niveau 70", valeur: 70},
     {badgeName : "Niv80", badgesrc:"./badge/80.png", badgeDesc:"Atteindre le niveau 80", valeur: 80},
     {badgeName : "Niv90", badgesrc:"./badge/90.png", badgeDesc:"Atteindre le niveau 90", valeur: 90},
-    {badgeName : "Niv100", badgesrc:"./badge/100.png", badgeDesc:"Atteindre le niveau 100", valeur: 100}
+    {badgeName : "Niv100", badgesrc:"./badge/100.png", badgeDesc:"Atteindre le niveau 100", valeur: 100},
+    {badgeName : "choucroute", badgesrc:"./badge/choucroute.png", badgeDesc:"25000 points sur Strasbourg en 1s", valeur: "choucroute"},
+    {badgeName : "halsacien", badgesrc:"./badge/halsacien.png", badgeDesc:"25000 points sur Strasbourg", valeur: "halsacien"},
+    {badgeName : "globetrotter", badgesrc:"./badge/globetrotter.png", badgeDesc:"15000 points sur World", valeur: "globetrotter"},
+    {badgeName : "conqueror", badgesrc:"./badge/conqueror.png", badgeDesc:"20000 points sur World en 1s", valeur: "conqueror"},
+    {badgeName : "croissant", badgesrc:"./badge/croissant.png", badgeDesc:"20000 points sur France", valeur: "croissant"},
+    {badgeName : "marine", badgesrc:"./badge/marine.png", badgeDesc:"20000 points sur France en 1s", valeur: "marine"},
+    {badgeName : "voyageur", badgesrc:"./badge/voyageur.png", badgeDesc:"20000 points sur Europe", valeur: "voyageur"},
+    {badgeName : "euro", badgesrc:"./badge/euro.png", badgeDesc:"15000 points sur Europe en 1s", valeur: "euro"}
 ]
