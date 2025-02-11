a59f13ddd9eaa77f55e56c43a656367cbba7b4b4
SamuelTussing <sml.tussing@gmail.com>
2025-02-11 10:37:43 +0100

    mise a jour

--- public/script.js
+++ public/script.js
@@ -414,7 +414,7 @@ function showBadgeNotification(unlockedBadges) {
     unlockedBadges.forEach((badge, index) => {
         setTimeout(() => {
             // Met à jour l'image, le titre et le texte du badge
-            popupimg.src = `/assets/badges/${badge.toLowerCase().replace(/ /g, "_")}.png`;
+            popupimg.src = `/assets/badge/${badge.toLowerCase().replace(/ /g, "_")}.png`;
             popuptitre.textContent = "🎖️ Nouveau badge débloqué !";
             popuptext.textContent = badge;
 
