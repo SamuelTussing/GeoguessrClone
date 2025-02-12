c1104f9dd9b0ce12dc016c8f5a426114c83051ce
SamuelTussing <sml.tussing@gmail.com>
2025-02-12 11:43:58 +0100

    ajout d'un badge

--- public/badgeList.js
+++ public/badgeList.js
@@ -27,5 +27,6 @@ const badgeList = [
     {badgeName : "Reporter", badgesrc:"./badge/Reporter.png", badgeDesc:"Réaliser un score de 15000 points sur Famous", valeur: "Reporter"},
     {badgeName : "Duc-de-Agass", badgesrc:"./badge/DucdeAgass.png", badgeDesc:"Réaliser un score de 15000 points sur Capitales", valeur: "DucdeAgass"},
     {badgeName : "Routard Pro", badgesrc:"./badge/RoutardPro.png", badgeDesc:"Réaliser un score de 20000 points sur Capitales", valeur: "RoutardPro"},
-    {badgeName : "Rien", badgesrc:"./badge/Rien.png", badgeDesc:"????", valeur: "Rien"}
+    {badgeName : "Routard Pro", badgesrc:"./badge/RoutardPro.png", badgeDesc:"Réaliser un score de 20000 points sur Capitales", valeur: "RoutardPro"},
+    {badgeName : "Accompli", badgesrc:"./badge/Accompli.png", badgeDesc:"Posséder tous les autres badges", valeur: "Accompli"}
 ]
